f12cee168cd50f0faad3ff4f7e291e013edb31d4
ServandoT <gonzalez.agar.carlos@iescamas.es>
2025-04-07 20:26:13 +0200

    Inicio de proyecto

--- src/main/java/com/proyectointegrado/skillswap/ServletInitializer.java
+++ src/main/java/com/proyectointegrado/skillswap/ServletInitializer.java
@@ -7,6 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        TODO comentario de prueba
         return application.sources(SkillSwapApplication.class);
     }
 
