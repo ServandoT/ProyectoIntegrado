1470422ccdfadb5aed160db75df5dc2a8920f99a
ServandoT <gonzalez.agar.carlos@iescamas.es>
2025-04-21 20:34:47 +0200

    Añadido controlador Clase

---
+++ src/main/java/com/proyectointegrado/skillswap/controladores/ClaseControlador.java
@@ -0,0 +1,99 @@
+package com.proyectointegrado.skillswap.controladores;
+
+import com.proyectointegrado.skillswap.conf.JwtService;
+import com.proyectointegrado.skillswap.entidades.Clase;
+import com.proyectointegrado.skillswap.entidades.Usuario;
+import com.proyectointegrado.skillswap.repositorios.UsuarioRepositorio;
+import com.proyectointegrado.skillswap.servicios.ClaseServicioImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@RestController()
+@RequestMapping("/clases")
+public class ClaseControlador {
+
+    private final ClaseServicioImpl claseServicio;
+    private final JwtService jwtService;
+    private final UsuarioRepositorio usuarioRepositorio;
+
+    public ClaseControlador(ClaseServicioImpl claseServicio, JwtService jwtService, UsuarioRepositorio usuarioRepositorio) {
+        this.claseServicio = claseServicio;
+        this.jwtService = jwtService;
+        this.usuarioRepositorio = usuarioRepositorio;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> listarClases() {
+        return ResponseEntity.ok(claseServicio.obtenerClases());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> crearClase(@RequestBody Clase clase, HttpServletRequest request) {
+
+        String email = jwtService.extractUsername(getToken(request));
+        Usuario usuario = usuarioRepositorio.findByEmail(email).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+//        TODO falta algo?
+        clase.setProfesor(usuario);
+
+        return ResponseEntity.ok(claseServicio.guardarClase(clase));
+    }
+
+//    TODO implementar que también pueda si es admin
+    @DeleteMapping
+    public ResponseEntity<?> eliminarClase(@RequestParam Long id, HttpServletRequest request) {
+
+        String email = jwtService.extractUsername(getToken(request));
+        Usuario usuario = usuarioRepositorio.findByEmail(email).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        Optional<Clase> clase = claseServicio.obtenerClase(id);
+
+        if (clase.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        Long idProfesor = clase.get().getProfesor().getId();
+        Long idUsuario = usuario.getId();
+
+        if (!idProfesor.equals(idUsuario)) return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+
+        claseServicio.eliminarClase(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PutMapping
+    public ResponseEntity<?> actualizarClase(@RequestParam Long id, @RequestBody Clase clase, HttpServletRequest request) {
+
+        String email = jwtService.extractUsername(getToken(request));
+        Usuario usuario = usuarioRepositorio.findByEmail(email).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        Optional<Clase> claseExistente = claseServicio.obtenerClase(id);
+
+//        TODO Necesito el id del existente?
+        if (claseExistente.isPresent() && claseExistente.get().getProfesor().getId().equals(usuario.getId())) {
+            Clase claseNueva = Clase.builder()
+                    .id(clase.getId())
+                    .titulo(clase.getTitulo())
+                    .categoria(clase.getCategoria())
+                    .precio(clase.getPrecio())
+                    .profesor(clase.getProfesor())
+                    .idioma(clase.getIdioma())
+                    .valoraciones(clase.getValoraciones())
+                    .build();
+
+            return ResponseEntity.ok(claseServicio.guardarClase(claseNueva));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    private String getToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        return authHeader != null && authHeader.startsWith("Bearer ")
+                ? authHeader.substring(7)
+                : null;
+    }
+}
