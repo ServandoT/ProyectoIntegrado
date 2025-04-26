daec8632f6db2643aed6e409a00592e0977d0adc
ServandoT <gonzalez.agar.carlos@iescamas.es>
2025-04-26 13:31:47 +0200

    Implementado Swagger

--- src/main/java/com/proyectointegrado/skillswap/conf/JwtAuthenticationFilter.java
+++ src/main/java/com/proyectointegrado/skillswap/conf/JwtAuthenticationFilter.java
@@ -14,9 +14,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -24,6 +27,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private static final List<String> PUBLIC_URLS = List.of(
+            "/api/v1/auth",
+            "/v3/api-docs",
+            "/v3/api-docs/",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/webjars/**"
+    );
+    private static final AntPathMatcher antPathMatcher = new AntPathMatcher();
+    private final PathMatcher pathMatcher;
 
     @Override
     protected void doFilterInternal(
@@ -31,6 +45,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+
+        String path = request.getServletPath();
+
+        if (PUBLIC_URLS.stream().anyMatch(p -> pathMatcher.match(p, path))) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
--- src/main/java/com/proyectointegrado/skillswap/demo/DemoControlador.java
+++ src/main/java/com/proyectointegrado/skillswap/demo/DemoControlador.java
@@ -1,10 +1,12 @@
 package com.proyectointegrado.skillswap.demo;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/api/v1/demo")
 public class DemoControlador {
