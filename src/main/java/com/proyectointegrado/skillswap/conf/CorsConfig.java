9f47675b1b40bb9da40acb1d9999dacaf9132319
ServandoT <gonzalez.agar.carlos@iescamas.es>
2025-05-08 11:51:19 +0200

    Cambios CORS (todos los orígenes)

--- src/main/java/com/proyectointegrado/skillswap/conf/CorsConfig.java
+++ src/main/java/com/proyectointegrado/skillswap/conf/CorsConfig.java
@@ -14,8 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:80")
-//                        .allowedOriginsPatterns("*")
+//                        .allowedOrigins("http://localhost:5173", "http://localhost:80")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
